79792f60fbcbff4ca00fb27bbf5a30749cb56db0
matiit <matiqing@gmail.com>
2012-10-15 18:15:49 +0200

    navbar woring, dropdown is not needed though

--- application/views/navbar.blade.php
+++ application/views/navbar.blade.php
@@ -6,16 +6,6 @@
       <li><a href="#">O mnie</a></li>
       <li><a href="#">Wszystkie projekty</a></li>
       <li><a href="#">Wykorzystywane technologie</a></li>
-      <li class="dropdown">
-        <a href="#" class="dropdown-toggle" data-toggle="dropdown">
-          MOCK
-          <b class="caret"></b>
-        </a>
-        <ul class="dropdown-menu">
-          <li><a href="#">Mock</a></li>
-          <li><a href="#">Mock</a></li>
-        </ul>
-      </li>
     </ul>
     <script type="text/javascript">
       $(document).ready( function() {
