95c151bfb3015e8265a563aa35d28395cc5c54c6
matiit <matiqing@gmail.com>
2012-10-15 13:39:23 +0200

    name of links

--- application/views/navbar.blade.php
+++ application/views/navbar.blade.php
@@ -2,9 +2,9 @@
 	<div class="navbar-inner">
 		{{ HTML::link('/','Main Page', array('class'=>'brand'))}}
 		<ul class="nav">
-			<li><a href="#">Mock</a></li>
-			<li><a href="#">Mock</a></li>
-			<li><a href="#">Mock</a></li>
+			<li><a href="#">O mnie</a></li>
+			<li><a href="#">Wszystkie projekty</a></li>
+			<li><a href="#">Technologie</a></li>
 			<li class="dropdown">
 				<a href="#" class="dropdown-toggle" data-toggle="dropdown">
 					XXXX
