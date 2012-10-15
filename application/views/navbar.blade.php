369b233df0eff3edcf62ab3268712db86d6de0f7
matiit <matiqing@gmail.com>
2012-10-15 14:03:56 +0200

    dropdown doesnT work ;/

--- application/views/navbar.blade.php
+++ application/views/navbar.blade.php
@@ -1,23 +1,20 @@
 <div class="navbar navbar-inverse">
-	<div class="navbar-inner">
-		{{ HTML::link('/','Main Page', array('class'=>'brand'))}}
-		<ul class="nav">
-			<li><a href="#">O mnie</a></li>
-			<li><a href="#">Wszystkie projekty</a></li>
-			<li><a href="#">Technologie</a></li>
-			<li class="dropdown">
-				<a href="#" class="dropdown-toggle" data-toggle="dropdown">
-					XXXX
-					<b class="caret"></b>
-				</a>
-				<ul class="dropdown-menu">
-					<li><a href="#">Mock</a></li>
-					<li><a href="#">Mock</a></li>
-					<li><a href="#">Mock</a></li>
-					<li><a href="#">Mock</a></li>
-					<li><a href="#">Mock</a></li>
-				</ul>
-			</li>
-		</ul>
-	</div>
-</div>
+  <div class="navbar-inner">
+    {{HTML::link('/', 'Strona Główna', array('class' =>'brand'))}}
+    <ul class="nav">
+      <!-- Class active need todo -->
+      <li><a href="#">O mnie</a></li>
+      <li><a href="#">Wszystkie projekty</a></li>
+      <li><a href="#">Wykorzystywane technologie</a></li>
+      <li class="dropdown">
+        <a href="#" class="dropdown-toggle" data-toggle="dropdown">
+          MOCK
+          <b class="caret"></b>
+        </a>
+        <ul class="dropdown-menu">
+          <li><a href="#">Mock</a></li>
+          <li><a href="#">Mock</a></li>
+        </ul>
+      </li>
+    </ul>
+  </div>
