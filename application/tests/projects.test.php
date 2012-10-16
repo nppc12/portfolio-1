61ce27641765bb8c0eb9d5c22858836d58572046
matiit <matiqing@gmail.com>
2012-10-16 11:38:59 +0200

    created test, maybe using selenium later..

---
+++ application/tests/projects.test.php
@@ -0,0 +1,13 @@
+<?php
+// application/tests/example.test.php
+class Project extends PHPUnit_Framework_TestCase {
+    /**
+     * Test that a given condition is met.
+     *
+     * @return void
+     */
+    public function testSomethingIsTrue()
+    {
+        $this->assertTrue(true);    
+    }
+}
