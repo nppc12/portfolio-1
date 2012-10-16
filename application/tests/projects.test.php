507db75f7956b85206bdd80f5a18240bb241920a
matiit <matiqing@gmail.com>
2012-10-16 16:29:12 +0200

    tests work now

--- application/tests/projects.test.php
+++ application/tests/projects.test.php
@@ -1,15 +1,11 @@
 <?php
 // application/tests/example.test.php
-class Project extends PHPUnit_Framework_TestCase {
+class ProjectTest extends PHPUnit_Framework_TestCase {
     /**
      * Test that a given condition is met.
      *
      * @return void
      */
 
-    public function testCreatingProject()
-    {
-   		$project = Project::create(array('name'=>'testName', 'desc'=> 'desc'));
-		$this->assertTrue($project->save());    
-    }
+    
 }
